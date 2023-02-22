fb3c31502bae56b148be16ba5813290c925f844f
Mayya <msalosina@mail.ru>
2023-02-22 12:27:55 +0700

    Ex_02 Queue

--- src/Ex_02.java
+++ src/Ex_02.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Ex_02 {
+import java.util.Arrays;
+
+public class Ex_02 {
+    public static void main(String[] args) {
+        MyQueue queue = new MyQueue();
+        queue.enqueue(8);
+        queue.enqueue(4);
+        queue.enqueue(2);
+        queue.enqueue(7);
+        System.out.println(queue.lListToString());
+        System.out.println(queue.first());      // 8
+        System.out.println(queue.dequeue());    // 8
+        System.out.println(queue.dequeue());    // 4
+        System.out.println(queue.first());      // 2
+    }
 }
---
+++ src/MyQueue.java
@@ -0,0 +1,33 @@
+// Реализуйте очередь с помощью LinkedList со следующими методами:
+// enqueue() - помещает элемент в конец очереди,
+// dequeue() - возвращает первый элемент из очереди и удаляет его,
+// first() - возвращает первый элемент из очереди, не удаляя.
+
+import java.util.LinkedList;
+
+public class Ex_02Queue {
+    private LinkedList<Integer> linkedList = new LinkedList<>();
+    // enqueue() - помещает элемент в конец очереди
+    void enqueue(int item) {
+        linkedList.addLast(item);
+    }
+    // dequeue() - возвращает первый элемент из очереди и удаляет его
+    Integer dequeue() {
+//        if(linkedList.size() > 0) {
+        if(!linkedList.isEmpty()) {
+            return linkedList.pollFirst();
+        }
+        return null;
+    }
+    // first() - возвращает первый элемент из очереди, не удаляя
+    Integer first() {
+        if(!linkedList.isEmpty()) {
+            return linkedList.peekFirst();
+        }
+        return null;
+    }
+
+    String lListToString() {
+        return linkedList.toString();
+    }
+}
